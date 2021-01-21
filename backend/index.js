105c1114587fd7220eb64b53e4f96b16c1a3e944
menezesmario <mario082@gmail.com>
2021-01-20 23:09:49 -0300

    view add product ok

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize');
 
 //Config
     //Template Engine
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 //Conexão com MYSQL    
 const sequelize = new Sequelize('recoffee', 'marioone', '123456', {
@@ -20,7 +25,7 @@ app.get('/cad', function(req, res){
 })
 
 app.post('/add_prod', function(req, res){
-    res.send("FORMULÁRIO RECEBIDO")
+    res.send("Nome: "+req.body.nome+" Descrição: "+req.body.descricao)
 })
 
 
